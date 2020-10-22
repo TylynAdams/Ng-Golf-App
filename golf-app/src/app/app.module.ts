5ec75a15e2c4f699a6498f955ee6f4a70a9a672a
Tylyn Adams <tyadams73@gmail.com>
2020-10-22 15:41:34 -0600

    I started a new app because my last one got too complicated. This is the simplified version.

---
+++ golf-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CourseComponent } from './components/course/course.component';
+import { ScorecardComponent } from './components/scorecard/scorecard.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseComponent,
+    ScorecardComponent,
+    WelcomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
